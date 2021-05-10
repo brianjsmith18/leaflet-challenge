f842a25cf001dac03599e27556dc9e36e73bf90e
Brian <brianjsmith18@gmail.com>
2021-05-10 19:54:12 -0400

    Legend

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -7,6 +7,15 @@ d3.json(queryUrl, function(data) {
   createFeatures(data.features);
 });
 
+function getColor(i) {
+    return i > 90 ? "#EA2C2C" :
+      i > 70  ? "#EA822C" :
+      i > 50  ? "#EE9C00" :
+      i > 30  ? "#EECC00" :
+      i > 10   ?  "#D4EE00" :
+              "#98EE00";
+    }
+
 function createFeatures(earthquakeData) {
 
   // Define a function we want to run once for each feature in the features array
@@ -19,7 +28,17 @@ function createFeatures(earthquakeData) {
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature, 
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius: feature.properties.mag*4,
+        fillColor: getColor(feature.geometry.coordinates[2]),
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    });
+  }
   });
 
   // Sending our earthquakes layer to the createMap function
@@ -71,4 +90,17 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+  var legend = L.control({ position: 'bottomright' });
+  legend.onAdd = function (myMap) {
+    var div = L.DomUtil.create('div', 'info legend'),
+      grades = [-10, 10, 30, 50, 70, 90]
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+  };
+  legend.addTo(myMap);
 }
